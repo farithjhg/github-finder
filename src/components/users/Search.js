42e6be57b62e71cc6e700fb41e92a8b6c7d7669f
fherasg <farith.heras@eurobits.es>
2020-12-09 00:48:38 +0100

    Implementing React App

---
+++ src/components/users/Search.js
@@ -0,0 +1,51 @@
+import React, { useState, useContext } from 'react';
+import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
+
+const Search = () => {
+  const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+
+  const [text, setText] = useState('');
+
+  const onChange = (e) => setText(e.target.value);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (text === '') {
+      alertContext.setAlert('Please enter some text!', 'light');
+    } else {
+      githubContext.searchUsers(text);
+      setText('');
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="text"
+          name="text"
+          placeholder="Search Users..."
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+        {githubContext.users.length > 0 && (
+          <button
+            className="btn-light btn-block"
+            onClick={githubContext.clearUsers}
+          >
+            Clear
+          </button>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default Search;
